2744783202fa530134f4dd2d92618830671fbee2
hamzalamin <hamzalamin80@gmail.com>
2024-12-25 11:33:23 +0100

    Add responsive navbar with hover effect and smooth transitions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import {SurveysListComponent} from './features/components/survey/surveys-list/surveys-list.component';
+import {NavbarComponent} from './features/components/shared/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [SurveysListComponent],
+  imports: [SurveysListComponent, NavbarComponent],
   templateUrl: './app.component.html',
   standalone: true,
   styleUrl: './app.component.css'
